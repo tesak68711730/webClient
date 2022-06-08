bf27a06a3acd5f2dd18e7beaa3594b243f8d9739
justin_st1 <justin_st1@mail.ru>
2022-06-08 12:36:52 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	TodoClient todoClient() throws Exception {
+		WebClient webClient = WebClient.builder()
+				.baseUrl("https://jsonplaceholder.typicode.com")
+				.build();
+		HttpServiceProxyFactory factory = new HttpServiceProxyFactory(new WebClientAdapter(webClient));
+		factory.afterPropertiesSet();
+		return factory.createClient(TodoClient.class);
+	}
+
+	record Todo(Long id, String title, Boolean completed, Long userId) {}
+
+	@HttpExchange("/todos")
+	interface TodoClient {
+		@GetExchange
+		List<Todo> todos();
+		@PostExchange
+		Todo createTodo(@RequestBody Todo request);
+		@GetExchange("/{todoID}")
+		Todo getTodoByID(@PathVariable("todoID") Long id);
+	}
+
+	@Bean
+	ApplicationRunner applicationRunner (TodoClient todoClient) {
+		return args -> {
+//			System.out.println(todoClient.todos());
+			Todo todo = new Todo(null, "asdasd", true, 1L);
+			Todo todo1 = null;
+			Todo response = todoClient.createTodo(todo);
+			System.out.println(todo1.id());
+//			System.out.println(todoClient.getTodoByID(response.id()));
+		};
+	}
+}
